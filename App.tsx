8a8bcf8172be7908e1a6bb84d15da89adfaf15a7
Wesley Lima <wesleyguedes7@gmail.com>
2024-05-07 14:55:47 -0300

    Instalação e configuração do styled-components e desenvolvimento do arquivo de tema

--- App.tsx
+++ App.tsx
@@ -1,21 +1,46 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+} from "@expo-google-fonts/montserrat";
+import { ThemeProvider } from "styled-components/native";
+
+import THEME from "./src/global/styles/theme";
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Synchrone</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={THEME}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Synchrone</Text>
+        <StatusBar backgroundColor={THEME.colors.primary} barStyle="default" />
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
+    backgroundColor: THEME.colors.primary,
+    flex: 1,
     justifyContent: "center",
+  },
+  text: {
+    color: THEME.colors.light,
+    fontFamily: THEME.fonts.semiBold,
+    fontSize: THEME.fontSize.largest,
   },
 });
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,27 @@
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default {
+  colors: {
+    primary: "#1E293B",
+    secondary: "#0284C7",
+    secondary_light: "#0284C71A",
+    dark: "#0A0A0A",
+    light: "#FAFAFA",
+    gray: "##CBD5E1",
+    danger: "#EF4444",
+    warning: "#FACC15",
+    success: "#16A34A",
+  },
+  fonts: {
+    medium: "Montserrat_400Regular",
+    regular: "Roboto_700Bold",
+    semiBold: "Montserrat_600SemiBold",
+  },
+  fontSize: {
+    smallest: RFValue(10),
+    smaller: RFValue(12),
+    normal: RFValue(14),
+    larger: RFValue(16),
+    largest: RFValue(18),
+  },
+};
