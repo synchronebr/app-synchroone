74825ab108ee58ed9b071f375bbeb23a292c3bbf
Wesley Lima <wesleyguedes7@gmail.com>
2025-03-05 18:17:23 -0300

    Implementação do OneSignal

--- src/screens/MeasurementPointDetails/index.tsx
+++ src/screens/MeasurementPointDetails/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Entypo } from "@expo/vector-icons";
+import { Entypo, FontAwesome } from "@expo/vector-icons";
 import { useTheme } from "styled-components/native";
 import { useNavigation } from "@react-navigation/native";
 
-import { LastMeasurementCard } from "../../components/LastMeasurementCard";
+import { PieChart } from "react-native-svg-charts";
 
 import {
   Container,
@@ -15,14 +15,56 @@ import {
   Subtitle,
   Content,
   Text,
+  Card,
+  Info,
+  InfoData,
+  CardTitle,
+  CardText,
+  CardSubtitle,
+  TemperatureCard,
+  Temperature,
+  Graphics,
+  GraphicsButtons,
+  GraphicButton,
+  GraphicButtonText,
+  GraphicImage,
+  StatusHistoryContainer,
+  StatusHistory,
 } from "./styles";
 import { differenceInMinutes } from "date-fns";
+import { Dimensions } from "react-native";
 
 export function MeasurementPointDetails({ route, nabigation }) {
   const navigation = useNavigation();
-  const [timeInfo, setTimeInfo] = useState({ minutesSinceLast: 0, nextExecutionIn: 60 });
+  const [timeInfo, setTimeInfo] = useState({
+    minutesSinceLast: 0,
+    nextExecutionIn: 60,
+  });
 
   const THEME = useTheme();
+
+  const data = [275, 180, 150, 120, 100]; // Valores de exemplo
+  const colors = ["#2CA58D", "#F08700", "#E63946", "#1D3557", "#F4A261"]; // Cores para cada segmento
+
+  const pieData = data.map((value, index) => ({
+    value,
+    svg: { fill: colors[index] },
+    key: `pie-${index}`,
+  }));
+
+  const stackedBarData = {
+    labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"],
+    legend: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"],
+    data: [
+      {
+        data: [50, 60, 40, 30, 70, 60], // Parte inferior da barra
+      },
+      {
+        data: [40, 50, 30, 60, 50, 40], // Parte superior da barra
+      },
+    ],
+    barColors: ["#dfe4ea", "#ced6e0", "#a4b0be"],
+  };
 
   useEffect(() => {
     const calculateTime = () => {
@@ -54,7 +96,9 @@ export function MeasurementPointDetails({ route, nabigation }) {
       <Header>
         <Image
           resizeMode="cover"
-          source={{ uri: "https://synchroone.s3.amazonaws.com/white-mp-sensor.png"}}
+          source={{
+            uri: "https://synchroone.s3.amazonaws.com/white-mp-sensor.png",
+          }}
         />
 
         <Entypo
@@ -88,9 +132,104 @@ export function MeasurementPointDetails({ route, nabigation }) {
       </Header>
 
       <Content>
-        <Text>Última medição</Text>
+        <Card>
+          <CardTitle>Aceleração</CardTitle>
 
-        <LastMeasurementCard item={route.params.item} />
+          <Info>
+            <InfoData>
+              <CardText>Axial</CardText>
+              <CardSubtitle>2.32 m/s</CardSubtitle>
+            </InfoData>
+
+            <InfoData>
+              <CardText>Vertical</CardText>
+              <CardSubtitle>2.32 m/s</CardSubtitle>
+            </InfoData>
+
+            <InfoData>
+              <CardText>Horizontal</CardText>
+              <CardSubtitle>2.32 m/s</CardSubtitle>
+            </InfoData>
+          </Info>
+        </Card>
+
+        <Card>
+          <CardTitle>Velocidade</CardTitle>
+
+          <Info>
+            <InfoData>
+              <CardText>Axial</CardText>
+              <CardSubtitle>2.32 m/s</CardSubtitle>
+            </InfoData>
+
+            <InfoData>
+              <CardText>Vertical</CardText>
+              <CardSubtitle>2.32 m/s</CardSubtitle>
+            </InfoData>
+
+            <InfoData>
+              <CardText>Horizontal</CardText>
+              <CardSubtitle>2.32 m/s</CardSubtitle>
+            </InfoData>
+          </Info>
+        </Card>
+
+        <TemperatureCard>
+          <CardTitle>Temperatura</CardTitle>
+
+          <Temperature>
+            <FontAwesome
+              name="thermometer-three-quarters"
+              size={24}
+              color={THEME.colors.primary}
+            />
+
+            <CardTitle>43 C</CardTitle>
+          </Temperature>
+        </TemperatureCard>
+
+        <Text>Gráficos</Text>
+
+        <Graphics>
+          <GraphicsButtons>
+            <GraphicButton>
+              <GraphicButtonText>Aceleração RMS</GraphicButtonText>
+            </GraphicButton>
+
+            <GraphicButton>
+              <GraphicButtonText>Velocidade RMS</GraphicButtonText>
+            </GraphicButton>
+
+            <GraphicButton>
+              <GraphicButtonText>Temperatura Média</GraphicButtonText>
+            </GraphicButton>
+          </GraphicsButtons>
+
+          <GraphicImage
+            resizeMode="container"
+            source={require("../../assets/images/graphic-1.png")}
+          />
+
+          <StatusHistoryContainer>
+            <StatusHistory>Histórico de Status</StatusHistory>
+          </StatusHistoryContainer>
+
+          <PieChart
+            style={{ height: 200 }}
+            data={pieData}
+            innerRadius={50} // Define a aparência de "rosca"
+            outerRadius={"95%"}
+          />
+
+          <StatusHistoryContainer>
+            <StatusHistory>Raio-X de Monitoramento</StatusHistory>
+          </StatusHistoryContainer>
+
+          <GraphicImage
+            resizeMode="container"
+            source={require("../../assets/images/graphic-3.png")}
+          />
+        </Graphics>
       </Content>
     </Container>
   );
--- src/screens/MeasurementPointDetails/styles.ts
+++ src/screens/MeasurementPointDetails/styles.ts
@@ -1,6 +1,6 @@
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
-import { rgba } from 'polished';
+import { rgba } from "polished";
 
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.light};
@@ -15,16 +15,16 @@ export const Image = styled.Image`
 `;
 
 interface IAsset {
-  status: 'S' | 'W' | 'D';
+  status: "S" | "W" | "D";
 }
 export const Asset = styled.View`
   background-color: ${({ theme, status }) => {
     switch (status) {
-      case 'S':
+      case "S":
         return rgba(22, 163, 74, 0.8);
-      case 'W':
+      case "W":
         return rgba(250, 204, 21, 1);
-      case 'D':
+      case "D":
         return rgba(238, 68, 68, 0.8);
       default:
         return rgba(30, 41, 59, 0.8);
@@ -59,6 +59,8 @@ export const Subtitle = styled.Text`
 
 export const Content = styled.ScrollView.attrs({
   contentContainerStyle: {
+    gap: 16,
+    marginTop: 16,
     paddingHorizontal: 20,
   },
 })``;
@@ -69,4 +71,101 @@ export const Text = styled.Text`
   font-size: ${({ theme }) => theme.fontSize.normal}px;
   line-height: 21px;
   margin: 16px 0;
+`;
+
+export const Card = styled.View`
+  background-color: rgba(2, 132, 199, 0.1);
+  border-radius: 8px;
+  padding: 8px 12px;
+`;
+
+export const CardTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.dark};
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${({ theme }) => theme.fontSize.normal}px;
+  line-height: 21px;
+`;
+
+export const CardText = styled.Text`
+  color: ${({ theme }) => theme.colors.primary};
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${({ theme }) => theme.fontSize.normal}px;
+  line-height: 21px;
+`;
+
+export const CardSubtitle = styled.Text`
+  color: ${({ theme }) => theme.colors.dark};
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${({ theme }) => theme.fontSize.normal}px;
+  line-height: 21px;
+`;
+
+export const Info = styled.View`
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+export const InfoData = styled.View``;
+
+export const TemperatureCard = styled.View`
+  align-items: center;
+  background-color: rgba(2, 132, 199, 0.1);
+  border-radius: 8px;
+  gap: 8px;
+  padding: 8px 12px;
+`;
+
+export const Temperature = styled.View`
+  align-items: center;
+  flex-direction: row;
+  gap: 4px;
+`;
+
+export const Graphics = styled.View`
+  margin-bottom: 42px;
+`;
+
+export const GraphicsButtons = styled.View`
+  flex-direction: row;
+  gap: 5px;
+  justify-content: space-between;
+  margin-bottom: 5px;
+`;
+
+export const GraphicButton = styled.TouchableOpacity`
+  background-color: rgba(241, 245, 249, 1);
+  border-radius: 8px;
+  flex: 1;
+  padding: 8px 18px;
+`;
+
+export const GraphicButtonText = styled.Text`
+  color: rgba(30, 41, 59, 0.6);
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${({ theme }) => theme.fontSize.smallest};
+  line-height: 20px;
+  text-align: center;
+`;
+
+export const GraphicImage = styled.Image`
+  height: ${RFValue(300)}px;
+  margin-bottom: 16px;
+  width: 100%;
+`;
+
+export const StatusHistoryContainer = styled.View`
+  background-color: ${({ theme }) => theme.colors.light};
+  border-color: rgba(241, 245, 249, 1);
+  border-radius: 8px;
+  border-width: 10px;
+  margin-bottom: 5px;
+  padding: 8px;
+`;
+
+export const StatusHistory = styled.Text`
+  color: ${({ theme }) => theme.colors.primary};
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${({ theme }) => theme.fontSize.largest};
+  font-weight: bold;
+  text-align: center;
 `;
