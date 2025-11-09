b500f37a71421d749943b34e12e7c58ad1b2dd39
Vitor Bonet <vitorbonet@hotmail.com>
2025-11-09 19:13:13 -0300

    dispositivos empty

--- src/screens/Devices/index.tsx
+++ src/screens/Devices/index.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo, useState } from "react";
 import ContentLoader, { Rect } from "react-content-loader/native";
 import { useWindowDimensions } from "react-native";
+import { Toast } from "react-native-toast-notifications";
 import { t } from "i18next";
 
 import { 
@@ -17,6 +18,8 @@ import Tabs from "../../components/Pages/Tabs";
 import { Button } from "../../components/Button";
 import DeviceCard from "../../components/Pages/Devices/DeviceCard";
 import { useNavigation } from "@react-navigation/native";
+import { EmptyContainer } from "../Home/styles";
+import EmptyState from "../../components/EmptyState";
 
 const H_PADDING = 20;     // mesmo padding horizontal do <Content />
 const CARD_HEIGHT = 80;   // altura de cada card
@@ -80,24 +83,32 @@ export function Devices() {
           {isLoading ? (
             <SkeletonDevices width={contentWidth} rows={6} />
           ) : (
-            <ContentCards>
-              <DeviceCard device={{ code: 'SSYNC-AAA1', battery: 100, readingWindow: 10, status: '', connectionStatus: 'ON' }}  signal="L" type={tabs} />
-              <DeviceCard device={{ code: 'SSYNC-AAA2', battery: 100, readingWindow: 10, status: '', connectionStatus: 'OF' }}  signal="M" type={tabs} />
-              <DeviceCard device={{ code: 'SSYNC-AAA3', battery: 100, readingWindow: 10, status: '', connectionStatus: 'NC' }}  signal="H" type={tabs} />
-              <DeviceCard device={{ code: 'SSYNC-AAA4', battery: 100, readingWindow: 10, status: '', connectionStatus: 'NG' }}  signal="H" type={tabs} />
-              <DeviceCard device={{ code: 'SSYNC-AAA5', battery: 100, readingWindow: 10, status: '', connectionStatus: 'NC' }}  signal="H" type={tabs} />
-              <DeviceCard device={{ code: 'SSYNC-AAA6', battery: 100, readingWindow: 10, status: '', connectionStatus: 'NC' }}  signal="H" type={tabs} />
-              <DeviceCard device={{ code: 'SSYNC-AAA7', battery: 100, readingWindow: 10, status: '', connectionStatus: 'NC' }}  signal="H" type={tabs} />
-            </ContentCards>
+            // <ContentCards>
+            //   <DeviceCard device={{ code: 'SSYNC-AAA1', battery: 100, readingWindow: 10, status: '', connectionStatus: 'ON' }}  signal="L" type={tabs} />
+            //   <DeviceCard device={{ code: 'SSYNC-AAA2', battery: 100, readingWindow: 10, status: '', connectionStatus: 'OF' }}  signal="M" type={tabs} />
+            //   <DeviceCard device={{ code: 'SSYNC-AAA3', battery: 100, readingWindow: 10, status: '', connectionStatus: 'NC' }}  signal="H" type={tabs} />
+            //   <DeviceCard device={{ code: 'SSYNC-AAA4', battery: 100, readingWindow: 10, status: '', connectionStatus: 'NG' }}  signal="H" type={tabs} />
+            //   <DeviceCard device={{ code: 'SSYNC-AAA5', battery: 100, readingWindow: 10, status: '', connectionStatus: 'NC' }}  signal="H" type={tabs} />
+            //   <DeviceCard device={{ code: 'SSYNC-AAA6', battery: 100, readingWindow: 10, status: '', connectionStatus: 'NC' }}  signal="H" type={tabs} />
+            //   <DeviceCard device={{ code: 'SSYNC-AAA7', battery: 100, readingWindow: 10, status: '', connectionStatus: 'NC' }}  signal="H" type={tabs} />
+            // </ContentCards>
+            <>
+            <EmptyContainer>
+                <EmptyState
+                  title={t('index.deviceConfiguration')}
+                  description={t('index.errorLoadDevices')}
+                />
+            </EmptyContainer>
+            </>
           )}
         </ScrollArea>
 
         <Footer>
-          <Button title={t('index.newDevice')} onPress={() => {
-            navigation.navigate("DeviceSetupStack", {
-              screen: "NewDevice1",
-            });
-          }}/>
+          {/* <Button title={t('index.newDevice')} onPress={() => {
+            // navigation.navigate("DeviceSetupStack", {
+            //   screen: "NewDevice1",
+            // });
+          }}/> */}
         </Footer>
       </Content>
     </Container>
